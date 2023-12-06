ca8db22ec17a5625b44169850db6dc6b5c544e5a
Jamarley <chris.e.ho@protonmail.com>
2023-12-06 23:42:59 +0100

    Finished Day 4

--- Day-04/haskell/jamarley/solution2.hs
+++ Day-04/haskell/jamarley/solution2.hs
@@ -1,19 +1,3 @@
-{-
-        [1,1,1,1,1,1]
-        [1,2,2,2,2,1]
-        [1,2,4,4,2,1]
-
-        add always one step forward:
-        list = take x . listCount
-        rec x xs
-                | x > 0 = rec (x - 1) (map (+) xs list) 
-                | otherwise = (map (+) xs list)
-        rec _ _ = []
-
-        zipWith (+) orig list
--}
-
-
 import Data.List.Split (splitOn)
 
 sanitize :: String -> [[Int]]
@@ -30,20 +14,18 @@ countDuplicates _ = []
 extend :: [Int] -> [Int] -> [Int]
 extend _ [] = []
 extend [] _ = []
-extend (x:xs) (y:ys)
-        | head ys > 1 = y : extend xs (zipWithRest (^) (take x ys) ys)
-        | otherwise = y : extend xs (zipWithRest (+) (take x ys) ys)
+extend (x:xs) (y:ys) = y : extend xs (zipWithRest (map (+y) (take x ys)) ys)
 
-zipWithRest :: (Int -> Int -> Int) -> [Int] -> [Int] -> [Int]
-zipWithRest _ [] xs = xs
-zipWithRest _ xs [] = xs 
-zipWithRest p (x:xs) (y:ys) = p x y : zipWithRest p xs ys 
+zipWithRest :: [Int] -> [Int] -> [Int]
+zipWithRest _ [] = []
+zipWithRest [] ys = ys
+zipWithRest (x:xs) (y:ys) = x : zipWithRest xs ys
 
 main :: IO ()
 main = do
-        content <- readFile "inputtest"
+        content <- readFile "input"
         let input = lines content
             sanitizedInput = map sanitize input
             listCount = take (length $ map dropSecond sanitizedInput) [1,1..] :: [Int]
         print (countDuplicates sanitizedInput)
-        print (extend (countDuplicates sanitizedInput) listCount)
+        print (sum $ extend (countDuplicates sanitizedInput) listCount)
